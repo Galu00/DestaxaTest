f6c05a22cdb579e69cb6120d7e05718c4239ab88
Galu00 <gabrieldelimagalindo.gdlg@gmail.com>
2025-07-10 23:29:29 -0300

    feat(authorizer v2):
    - change ISO.

--- src/main/java/com/destaxa/AuthorizationRules.java
+++ src/main/java/com/destaxa/AuthorizationRules.java
@@ -1,43 +1,47 @@
 package com.destaxa;
 
+import org.jpos.iso.ISOMsg;
+
 public class AuthorizationRules {
 
-    public static ISOMessage process(ISOMessage request) {
-        String valorStr = request.getField(4);
+    private static final String XML_LAYOUT = "iso87ascii.xml";
+
+    public static ISOMsg process(ISOMsg request) throws Exception {
+        String valorStr = request.getString(4);
         if (valorStr == null || valorStr.isEmpty()) return null;
 
         long valor = Long.parseLong(valorStr);
 
-        if (valor > 100000) {
-            return null;
-        }
+        if (valor > 100000) return null;
 
-        ISOMessage response = new ISOMessage("0210");
+        ISOMsg response = new ISOMsg();
+        response.setMTI("0210");
 
-        response.setField(4, valorStr);
-        response.setField(7, request.getField(7));
-        response.setField(11, request.getField(11));
-        response.setField(12, request.getField(12));
-        response.setField(13, request.getField(13));
-        response.setField(42, request.getField(42));
+        response.set(4, valorStr);
+        response.set(7, request.getString(7));
+        response.set(11, request.getString(11));
+        response.set(12, request.getString(12));
+        response.set(13, request.getString(13));
+        response.set(42, request.getString(42));
 
         if (valor % 2 == 0) {
-            response.setField(39, "000");
-            response.setField(38, createAuthCod());
+            response.set(39, "000");
+            response.set(38, generateAuthCode());
         } else {
-            response.setField(39, "051");
+            response.set(39, "051");
         }
 
-        response.setField(127, createNsuHost());
+        response.set(127, generateNSU());
 
         return response;
     }
 
-    private static String createAuthCod() {
+    private static String generateAuthCode() {
         return String.valueOf((int) (Math.random() * 900000 + 100000));
     }
 
-    private static String createNsuHost() {
+    private static String generateNSU() {
         return "NSU" + System.currentTimeMillis();
     }
 }
+
